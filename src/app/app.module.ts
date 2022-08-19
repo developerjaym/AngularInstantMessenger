b71ab6872616d5129927ae6a225c7991546c6bd6
Jay M <jaymansmann@yahoo.com>
2022-08-19 17:08:30 -0400

    chat window roughed in, nonfunctional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessageWindowComponent } from './messaging/component/message-window/message-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageWindowComponent
   ],
   imports: [
     BrowserModule,
