ce57a4960c433b6a813b527fe19bf29349d05bc0
Moe <m.ibrahim0228@gmail.com>
2022-08-26 14:54:30 -0400

    envioremtn links used

--- src/app/authenticate.service.ts
+++ src/app/authenticate.service.ts
@@ -24,19 +24,22 @@ export class AuthenticateService {
   signin(loginRequest: LoginRequest): Observable<boolean> {
     // 1
     return this.httpClient
-      .post<JwtAuthenticationResponse>(environment.usersLink + 'signin', loginRequest)
+      .post<JwtAuthenticationResponse>(
+        environment.usersLink + 'signin',
+        loginRequest
+      )
       .pipe(
         tap(
           (jwtAuthenticationResponse) =>
             (this.jwtAuthenticationResponse = jwtAuthenticationResponse)
         ),
-        map(response => true),
-        catchError(e => of(false))
+        map((response) => true),
+        catchError((e) => of(false))
       );
   }
 
-  getUsers():Observable<UserDTO[]>{
-    return this.httpClient.get<UserDTO[]>(environment.usersLink)
+  getUsers(): Observable<UserDTO[]> {
+    return this.httpClient.get<UserDTO[]>(environment.usersLink);
   }
 
   getAuthorizationHeaderValue(): string {
--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -3,41 +3,47 @@ import { Injectable } from '@angular/core';
 import { Observable, retry, switchMap, tap, timer } from 'rxjs';
 import { CreateMessage } from './messaging/model/create-message';
 import { Message } from './message';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessagesService {
-// First hard coded values
-  messages:Message[] =
-        [
-            {
-                "id": 1,
-                "createdAt": "2022-08-23T15:22:44.985173Z",
-                "content": "Goodbye World",
-                "user": {
-                    "id": 1,
-                    "username": "jay"
-                }
-            }
-        ];
-  constructor(private httpClient: HttpClient) { }
+  // First hard coded values
+  messages: Message[] = [
+    {
+      id: 1,
+      createdAt: '2022-08-23T15:22:44.985173Z',
+      content: 'Goodbye World',
+      user: {
+        id: 1,
+        username: 'jay',
+      },
+    },
+  ];
+  constructor(private httpClient: HttpClient) {}
 
-  getMessages(id:string|null):Observable<Message[]> {
-    return timer(1, 500)
-    .pipe(
-        switchMap(x => this.httpClient.get<Message[]>
-        (`http://localhost:8080/api/conversations/${id}/messages`)),
-        retry()
-        // can I retry this if it fails?
-        // can I share this so that new Subscribers don't kick off new Http calls?
-        // is there a way I can make this stop
-        //    like take updates while some condition is true?
+  getMessages(id: string | null): Observable<Message[]> {
+    return timer(1, 500).pipe(
+      switchMap((x) =>
+        this.httpClient.get<Message[]>(
+          environment.conversationLink + `${id}/messages`
+        )
+      ),
+      retry()
+      // can I retry this if it fails?
+      // can I share this so that new Subscribers don't kick off new Http calls?
+      // is there a way I can make this stop
+      //    like take updates while some condition is true?
     );
   }
 
-  sendMessage(newMessage:CreateMessage,conversationID:number){
-    this.httpClient.post((`http://localhost:8080/api/conversations/${conversationID}/messages`),newMessage)
-    .subscribe()
+  sendMessage(newMessage: CreateMessage, conversationID: number) {
+    this.httpClient
+      .post(
+        `http://localhost:8080/api/conversations/${conversationID}/messages`,
+        newMessage
+      )
+      .subscribe();
   }
 }
